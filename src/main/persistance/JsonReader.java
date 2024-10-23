9e6262498e7c528bebfee8b7f633897d1a2e1492
buyangdaffa <daffaeffendy2911@gmail.com>
2024-10-23 06:44:25 -0700

    implement the methods for JsonReader

--- src/main/persistance/JsonReader.java
+++ src/main/persistance/JsonReader.java
@@ -25,50 +25,117 @@ public class JsonReader {
     // EFFECTS: reads teams from file and returns them;
     // throws IOException if an error occurs reading data from file
     public List<Team> read() throws IOException {
-        // TODO: Implement this method
-        return null;
+        String jsonData = readFile(source);
+        JSONArray jsonArray = new JSONArray(jsonData);
+        return parseTeams(jsonArray);
     }
 
     // EFFECTS: reads source file as string and returns it
     private String readFile(String source) throws IOException {
-        // TODO: Implement this method
-        return null;
+        StringBuilder contentBuilder = new StringBuilder();
+
+        try (Stream<String> stream = Files.lines(Paths.get(source), StandardCharsets.UTF_8)) {
+            stream.forEach(contentBuilder::append);
+        }
+
+        return contentBuilder.toString();
     }
-    
+
     // EFFECTS: parses JSONArray of teams and returns it
     private List<Team> parseTeams(JSONArray jsonArray) {
-        // TODO: Implement this method
-        return null;
+        List<Team> teams = new ArrayList<>();
+
+        for (Object json : jsonArray) {
+            JSONObject teamJson = (JSONObject) json;
+            teams.add(parseTeam(teamJson));
+        }
+
+        return teams;
     }
 
     // EFFECTS: parses team from JSON object and returns it
     private Team parseTeam(JSONObject jsonObject) {
-        // TODO: Implement this method
-        return null;
+        String teamName = jsonObject.getString("teamName");
+        String coachName = jsonObject.getString("coachName");
+        int totalWins = jsonObject.getInt("totalWins");
+        int totalLosses = jsonObject.getInt("totalLosses");
+        int totalDraws = jsonObject.getInt("totalDraws");
+
+        Team team = new Team(teamName, coachName);
+        addPlayers(team, jsonObject);
+        teamSetter(totalWins, totalLosses, totalDraws, team);
+
+        return team;
     }
 
     // MODIFIES: team
     // EFFECTS: sets team's totalWins, totalLosses, and totalDraws to the given values
     private void teamSetter(int totalWins, int totalLosses, int totalDraws, Team team) {
-        // TODO: Implement this method
+        while (team.getTotalWins() < totalWins) {
+            team.incrementWins();
+        }
+        while (team.getTotalLosses() < totalLosses) {
+            team.incrementLosses();
+        }
+        while (team.getTotalDraws() < totalDraws) {
+            team.incrementDraws();
+        }
     }
-    
+
     // MODIFIES: team
     // EFFECTS: parses players from JSON object and adds them to team
     private void addPlayers(Team team, JSONObject jsonObject) {
-        // TODO: Implement this method
+        for (Object json : jsonObject.getJSONArray("players")) {
+            JSONObject nextPlayer = (JSONObject) json;
+            addPlayer(team, nextPlayer);
+        }
     }
-    
+
     // MODIFIES: team
     // EFFECTS: parses player from JSON object and adds it to team
     private void addPlayer(Team team, JSONObject jsonObject) {
-        // TODO: Implement this method
-    }
+        String name = jsonObject.getString("name");
+        String position = jsonObject.getString("position");
+        int age = jsonObject.getInt("age");
+        int height = jsonObject.getInt("height");
+        int jerseyNumber = jsonObject.getInt("jerseyNumber");
+        int minPlayed = jsonObject.getInt("minPlayed");
+        int totalGoals = jsonObject.getInt("totalGoals");
+        int totalAssists = jsonObject.getInt("totalAssists");
+        int appearances = jsonObject.getInt("appearances");
+        int yellowCards = jsonObject.getInt("yellowCards");
+        int redCards = jsonObject.getInt("redCards");
+        boolean isInjured = jsonObject.getBoolean("isInjured");
     
+        Player player = new Player(name, position, age, height, jerseyNumber);
+        playerSetter(minPlayed, totalGoals, totalAssists, appearances, yellowCards, 
+                     redCards, isInjured, player);
+
+        team.addPlayer(player);
+    }
+
     // MODIFIES: player
     // EFFECTS: sets player's minPlayed, totalGoals, totalAssists, appearances, yellowCards, redCards, and isInjured
     private void playerSetter(int minPlayed, int totalGoals, int totalAssists, int appearances, 
-                              int yellowCards, int redCards, boolean isInjured, Player player) {
-        // TODO: Implement this method
+                                         int yellowCards, int redCards, boolean isInjured, Player player) {
+        player.addMinPlayed(minPlayed);
+        while (player.getTotalGoals() < totalGoals) {
+            player.addGoal();
+        }
+        while (player.getTotalAssists() < totalAssists) {
+            player.addAssist();
+        }
+        while (player.getAppearances() < appearances) {
+            player.addAppearances();
+        }
+        while (player.getYellowCards() < yellowCards) {
+            player.addYellowCards();
+        }
+        while (player.getRedCards() < redCards) {
+            player.addRedCards();
+        }
+        if (player.getIsInjured() != isInjured) {
+            player.setIsInjured();
+        }
     }
 }
