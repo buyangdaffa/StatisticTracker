fd8058e2e6baea828e9a0a2de7f1f0ea0b31e3ec
buyangdaffa <daffaeffendy2911@gmail.com>
2024-10-23 07:28:17 -0700

    created JsonWriterTest and json files for testing the JsonWriter

---
+++ src/test/persistance/JsonWriterTest.java
@@ -0,0 +1,67 @@
+package persistance;
+
+import model.Player;
+import model.Team;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class JsonWriterTest extends JsonTest {
+
+    @Test
+    void testWriterInvalidFile() {
+        try {
+            List<Team> teams = List.of(new Team("Team1", "Coach1"));
+            JsonWriter writer = new JsonWriter("./data/my\0illegal:fileName.json");
+            writer.open();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testWriterEmptyTeam() {
+        try {
+            List<Team> teams = List.of();
+            JsonWriter writer = new JsonWriter("./data/testWriterEmptyTeam.json");
+            writer.open();
+            writer.write(teams);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterEmptyTeam.json");
+            teams = reader.read();
+            assertEquals(0, teams.size());
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+
+    @Test
+    void testWriterGeneralTeam() {
+        try {
+            List<Team> teams = List.of(new Team("Team1", "Coach1"));
+            teams.get(0).addPlayer(new Player("Player1", "CF", 20, 180, 9));
+            teams.get(0).addPlayer(new Player("Player2", "GK", 21, 190, 1));
+            JsonWriter writer = new JsonWriter("./data/testWriterGeneralTeam.json");
+            writer.open();
+            writer.write(teams);
+            writer.close();
+
+            JsonReader reader = new JsonReader("./data/testWriterGeneralTeam.json");
+            teams = reader.read();
+            assertEquals(1, teams.size());
+            Team team = teams.get(0);
+            checkTeam("Team1", "Coach1", 0, 0, 0, team);
+            List<Player> players = team.getPlayers();
+            assertEquals(2, players.size());
+            checkPlayer("Player1", "CF", 20, 180, 9, 0, 0, 0, 0, 0, 0, false, 0, 0, 0, players.get(0));
+            checkPlayer("Player2", "GK", 21, 190, 1, 0, 0, 0, 0, 0, 0, false, 0, 0, 0, players.get(1));
+        } catch (IOException e) {
+            fail("Exception should not have been thrown");
+        }
+    }
+}
