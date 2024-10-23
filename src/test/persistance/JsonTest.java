77fcbf4e73bd5cc3b87c4fbbabc3aaf63cf49615
buyangdaffa <daffaeffendy2911@gmail.com>
2024-10-23 06:00:48 -0700

    created JsonTest that includes checkPlayer and checkTeam for testing purposes

---
+++ src/test/persistance/JsonTest.java
@@ -0,0 +1,26 @@
+package persistance;
+
+import model.Player;
+import model.Team;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class JsonTest {
+    protected void checkPlayer(String name, String position, int age, int height, int jerseyNumber, 
+                               int minPlayed, int totalGoals, int totalAssists, int appearances, 
+                               int yellowCards, int redCards, boolean isInjured, int wins, int losses, 
+                               int draws, Player player) {
+        assertEquals(name, player.getName());
+        assertEquals(age, player.getAge());
+        assertEquals(height, player.getHeight());
+    }
+
+    protected void checkTeam(String teamName, String coachName, int totalWins,
+                             int totalLosses, int totalDraws, Team team) {
+        assertEquals(teamName, team.getTeamName());
+        assertEquals(coachName, team.getCoachName());
+        assertEquals(totalWins, team.getTotalWins());
+        assertEquals(totalLosses, team.getTotalLosses());
+        assertEquals(totalDraws, team.getTotalDraws());
+    }
+}
